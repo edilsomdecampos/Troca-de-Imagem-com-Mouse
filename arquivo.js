22ec910aeaa196bcbff7875c8409b28f224f4e12
Edilson de Campos da Silva <156320157+edilsomdecampos@users.noreply.github.com>
2025-03-24 16:49:55 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,7 @@
+function trocarImagem() {
+    document.getElementById('imagem').src = 'imagem/noite1.jpg';
+}
+
+function restaurarImagem() {
+    document.getElementById('imagem').src = 'imagem/dia2.jpg';
+}
